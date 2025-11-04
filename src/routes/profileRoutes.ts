3c2773d76e3ea0c44a504442adacf238ec53ea79
sheyh77 <ubabaniyozov@gmail.com>
2025-11-05 00:15:29 +0500

    edit profile image

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,34 +1,14 @@
 import express from "express";
-import multer from "multer";
 import Profile from "../models/profileModel.js";
 
 const router = express.Router();
 
-// Rasmni saqlash joyi (local yoki server)
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/"); // uploads papkasiga saqlanadi
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
-// POST /api/profile — foydalanuvchi ma'lumotlarini saqlash
-router.post("/", upload.single("image"), async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const { fullName, phone, email, address } = req.body;
-    const image = req.file ? `/uploads/${req.file.filename}` : "";
-
+    const { fullName, phone, email, address, image } = req.body;
     const newProfile = new Profile({ fullName, phone, email, address, image });
     await newProfile.save();
-
-    res.status(201).json({
-      message: "Profil saqlandi",
-      profile: newProfile,
-    });
+    res.status(201).json({ message: "Profil saqlandi", profile: newProfile });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Serverda xatolik", error });
